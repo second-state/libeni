c74f98b531b2c069185d5b2e67cdd165b5616f99
Yi Huang <yi@skymizer.com>
2018-04-27 06:43:54 +0000

    Move ceni.h to examples.

--- examples/main.c
+++ examples/main.c
@@ -1,7 +1,12 @@
-#include <ceni.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+
+typedef void*    eni_create_t(char* pArgStr);
+typedef uint64_t eni_gas_t(void* pFunctor);
+typedef char*    eni_run_t(void* pFunctor);
+typedef void     eni_destroy_t(void* pFunctor);
 
 int main(int argc, char* argv[])
 {
