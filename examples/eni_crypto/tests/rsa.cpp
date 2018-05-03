3a6e63d0da585733a657b874b53d587f48dbccba
Yi Huang <yi@skymizer.com>
2018-05-03 07:05:32 +0000

    Refactor to test wrapper functions for OpenSSL RSA encryption/decryption.

--- examples/eni_crypto/tests/rsa.cpp
+++ examples/eni_crypto/tests/rsa.cpp
@@ -1,11 +1,8 @@
 #include <eni_crypto.h>
 #include <cassert> // replace with a testing framework such as SkyPat
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 
 extern "C" {
-#include <openssl/bio.h>
-#include <openssl/err.h>
 #include <openssl/rsa.h>
 }
 
@@ -15,38 +12,29 @@ int main(int argc, char* argv[])
 {
   if (argc < 2)
     return -1;
-  fprintf(stderr, "strlen = %zu\n", ::strlen(argv[1]));
 
   RSA* key = RSA_generate_key(1024, 3, 0, 0);
   if (NULL == key)
     return 1;
-  fprintf(stderr, "RSA_size = %d\n", ::RSA_size(key));
 
-  unsigned char* orig = new unsigned char[::strlen(argv[1])];
-  ::memcpy(orig, argv[1], ::strlen(argv[1]));
-
-  unsigned char* encrypted = new unsigned char[::RSA_size(key)];
-  int ret = RSA_public_encrypt(::strlen(argv[1]), orig, encrypted, key,
-    RSA_PKCS1_PADDING);
-  if (-1 == ret)
+  // Case 1: RSA_public_encrypt + RSA_private_decrypt
+  const std::string msg(argv[1]);
+  std::string encrypted, decrypted;
+  if (!rsa::pub_encrypt(*key, msg, encrypted))
     return 2;
-  delete[] orig;
-
-  unsigned char* decrypted = new unsigned char[ret];
-  ret = RSA_private_decrypt(ret, encrypted, decrypted, key,
-    RSA_PKCS1_PADDING);
-  if (-1 == ret)
+  if (!rsa::priv_decrypt(*key, encrypted, decrypted))
     return 3;
-  delete[] encrypted;
+  std::cout << decrypted << std::endl;
+  assert((msg == decrypted));
+
+  // Case 2: RSA_private_encrypt + RSA_public_decrypt
+  if (!rsa::priv_encrypt(*key, msg, encrypted))
+    return 4;
+  if (!rsa::pub_decrypt(*key, encrypted, decrypted))
+    return 5;
+  std::cout << decrypted << std::endl;
+  assert((msg == decrypted));
+
   RSA_free(key);
-
-  char* result = new char[ret + 1];
-  ::memcpy(result, decrypted, ret);
-  result[ret] = '\0';
-  delete[] decrypted;
-
-  fprintf(stderr, "Original: %s, Result: %s\n", argv[1], result);
-  assert((0 == ::strcmp(result, argv[1])));
-  delete[] result;
   return 0;
 }
