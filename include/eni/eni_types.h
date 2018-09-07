4a0767c1508a9192d4fcd60e0ef614a037ebdd63
Yi Huang <yi@skymizer.com>
2018-09-07 07:10:27 +0000

    Add alias `eni::UInt` for `eni::u256`.

--- include/eni/eni_types.h
+++ include/eni/eni_types.h
@@ -17,16 +17,17 @@ using u64 = boost::multiprecision::number<
     boost::multiprecision::unchecked, void>
 >;
 using u128 = boost::multiprecision::uint128_t;
-using u256 = boost::multiprecision::uint256_t;
-using s256 = boost::multiprecision::int256_t;
 using u160 = boost::multiprecision::number<
   boost::multiprecision::cpp_int_backend<160, 160,
     boost::multiprecision::unsigned_magnitude,
     boost::multiprecision::unchecked, void>
 >;
+using u256 = boost::multiprecision::uint256_t;
+using s256 = boost::multiprecision::int256_t;
 
 using Bool = bool;
 using Int = s256;
+using UInt = u256;
 using Address = u160;
 
 } // namespace of eni
