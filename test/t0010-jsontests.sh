f0eaad74bd48a05fa2bd4c8420e16365081326c0
afg <afg@secondstate.io>
2019-08-21 22:45:31 +0800

    update comments in jsontests runner

--- test/t0010-jsontests.sh
+++ test/t0010-jsontests.sh
@@ -6,7 +6,8 @@
 #
 #==-----------------------------------------------------------------------==#
 
-# XXX refactor to something else
+# TODO: jsontests should be run refactored to run independently,
+#       i.e. directly from ctest(1)
 PATH=$T_BINDIR/tools/eni_cli:$PATH \
     LD_LIBRARY_PATH=$T_BINDIR:$LD_LIBRARY_PATH \
     python3 -m unittest discover -s jsontests -t .
